424dce1db534831cd9de918532cb2111365d7d18
Sato <119936087+SatoruNG@users.noreply.github.com>
2024-11-19 16:04:46 +0400

    Add files via upload
    Организация программ и методы строк

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+# пользовательский ввод
+my_string = input()
+# длина введенного текста
+print(len(my_string))
+# введенный текст в верхнем регистре
+print(my_string.upper())
+# введенный текст в нижнем регистре
+print(my_string.lower())
+# введенный текст без пробелов
+my_string = my_string.replace(" ", "")
+# первый символ введенного текста
+print(my_string[0])
+# последний символ введенного текста
+print(my_string[-1])

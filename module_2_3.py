348bc4e4dde9f339f019a1b6fee6023bed97331b
Sato <119936087+SatoruNG@users.noreply.github.com>
2024-11-25 05:19:03 +0400

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,15 @@
+my_list = input().strip('[]').split(", ") # [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+idx = 0
+
+my_list = list(my_list)
+while True:
+    if int(my_list[idx]) < 0:
+        break
+
+    elif int(my_list[idx]) > 0:
+        print(my_list[idx])
+        idx += 1
+        continue
+
+    else:
+        idx += 1

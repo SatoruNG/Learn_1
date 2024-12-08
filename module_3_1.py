dadad842be6a6dc217326034c8f17c3071889b76
Sato <119936087+SatoruNG@users.noreply.github.com>
2024-12-08 18:45:06 +0400

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,19 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+
+    root_word = root_word.lower()
+
+    for i in other_words:
+        if root_word.lower() in i.lower():
+            same_words.append(i)
+            continue
+
+        if i.lower() in root_word.lower():
+            same_words.append(i)
+
+    return same_words
+
+result_1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result_2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result_1)
+print(result_2)
